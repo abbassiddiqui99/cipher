7fcc6552e3c1b456dbd3578ca89551a70e970ef0
abbassiddiqui99 <49212607+abbassiddiqui99@users.noreply.github.com>
2020-10-05 22:28:55 +0500

    Add files via upload

---
+++ shift-cipher-master/scripts/main.js
@@ -0,0 +1,36 @@
+function encrypt() {
+  var str = document.getElementById("inputtext").value;
+  var amount = document.getElementById("key").value;
+  console.log(str);
+
+  if (document.getElementById("ctype").value == "0") {
+    alert("please select cipher type");
+  }
+
+  if (document.getElementById("ctype").value == "1") {
+    myCipher();
+  } else if (document.getElementById("ctype").value == "2") {
+    vencrypt();
+  } else if (document.getElementById("ctype").value == "3") {
+    enplay();
+  } else if (document.getElementById("ctype").value == "4") {
+    Scrypt();
+  } else if (document.getElementById("ctype").value == "5") {
+    RCipher();
+  }
+}
+
+function decrypt() {
+  if (document.getElementById("ctype").value == "1") {
+    console.log("Caesars");
+    Cdecrypt();
+  } else if (document.getElementById("ctype").value == "2") {
+    Vdecrypt();
+  } else if (document.getElementById("ctype").value == "3") {
+    deplay();
+  } else if (document.getElementById("ctype").value == "4") {
+    Sdecrypt();
+  } else if (document.getElementById("ctype").value == "5") {
+    rdeCipher();
+  }
+}
